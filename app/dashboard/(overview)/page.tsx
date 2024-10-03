27d2b92247c17eb03ae7ccdc8b1ebb7b84823a3c
stavan-vc <stavan.sanghvi@viitor.cloud>
2024-10-03 13:55:13 +0530

    feat: enhance metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,8 +6,13 @@ import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
+import { Metadata } from "next";
 import { Suspense } from "react";
 import CardWrapper from "../../ui/dashboard/cards";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default async function DashboardPage() {
   return (
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
 export default function Customers() {
   return <p>Customers Page</p>;
 }
